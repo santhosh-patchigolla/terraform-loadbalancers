e1d251b2ad55fbb7f6904c6b62a9642cfa279fe9
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-07 13:36:13 +0530

    sdc

---
+++ env-dev/dev-backend.tfvars
@@ -0,0 +1,7 @@
+bucket          = "b54-santhosh-terraform-remote-state"
+key             = "alb/dev/terraform.tfstate"                   
+region          = "us-east-1"
+
+
+
+# alb = application load balancer
---
+++ main.tf
@@ -0,0 +1,18 @@
+module "alb-public" {
+  source                      = "./vendor/modules/alb"
+  ENV                         = var.ENV
+  ALB_NAME                    = "roboshop-public-alb" 
+  INTERNAL                    = false
+}
+
+module "alb-private" {
+  source                      = "./vendor/modules/alb"
+  ENV                         = var.ENV
+  ALB_NAME                    = "roboshop-private-alb"
+  INTERNAL                    = true
+}
+
+
+
+
+# We are passing the variable in this way..
